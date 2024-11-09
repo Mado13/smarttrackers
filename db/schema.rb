ca96d8e30d322ee7142a1c2d0d3c314802a4ad36
Matan Amidor <matanamidor@voormedia.com>
2024-11-09 19:25:19 +0100

    Add user and roles

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_08_121856) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "role", default: 1, null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +9,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.destroy_all
+
+User.create!(
+  email: 'employee@smarttrackers.com',
+  password: 'Employee123',
+  password_confirmation: 'Employee123',
+  first_name: 'Lydia',
+  last_name: 'Darashenko',
+  role: 'employee'
+)
+
+User.create!(
+  email: 'manager@smarttrackers.com',
+  password: 'Manager123',
+  password_confirmation: 'Manager123',
+  first_name: 'Shantal',
+  last_name: 'Cohen',  
+  role: 'manager'
+)
