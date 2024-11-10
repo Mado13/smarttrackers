f78a821bbe479d51a5b52fe99a1216c2c03ef7e1
Matan Amidor <matanamidor@voormedia.com>
2024-11-10 22:56:36 +0100

    Schema with readings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_09_183041) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_10_120013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_09_183041) do
     t.integer "time_unit", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "readings", force: :cascade do |t|
+    t.bigint "gauge_id", null: false
+    t.decimal "value"
+    t.date "date"
+    t.boolean "approved", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gauge_id"], name: "index_readings_on_gauge_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +49,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_09_183041) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "readings", "gauges"
 end
