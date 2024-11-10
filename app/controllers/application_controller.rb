97bccaeaeabc60783677c73da219e9a6529b0210
Matan Amidor <matanamidor@voormedia.com>
2024-11-10 22:24:49 +0100

    Add flash turbo stream to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def turbo_stream_flash_message(type, message)
+    turbo_stream.prepend(
+      'flash_messages',
+      partial: 'shared/flash',
+      locals: { type: type, message: message }
+    )
+  end
 end
