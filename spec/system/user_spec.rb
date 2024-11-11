150f887e8d5a5e24102d2de0f5261a8109d3c60b
Matan Amidor <matanamidor@voormedia.com>
2024-11-11 13:23:57 +0100

    Move user spec from deprecated features to system tests

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe 'User login', type: :feature, js: true do
+RSpec.describe 'User login', type: :system do
   let(:user) { create(:user) }
-  
+
   before do
     user
     visit new_user_session_path
-  end
+    expect(page).to have_current_path(new_user_session_path)
+  end 
 
   context 'with valid credentials' do
     it 'signs user in successfully' do
       fill_in 'user[email]', with: user.email
       fill_in 'user[password]', with: user.password
       click_button 'Log in'
-      
+
       expect(page).to have_current_path(root_path, wait: 5)
     end
   end
@@ -23,7 +24,7 @@ RSpec.describe 'User login', type: :feature, js: true do
       fill_in 'user[email]', with: user.email
       fill_in 'user[password]', with: 'wrongpassword'
       click_button 'Log in'
-      
+
       expect(page).to have_content('Invalid Email or password')
       expect(page).to have_current_path(new_user_session_path, wait: 5)
     end
@@ -32,8 +33,7 @@ RSpec.describe 'User login', type: :feature, js: true do
       fill_in 'user[email]', with: 'nonexistent@example.com'
       fill_in 'user[password]', with: 'password123'
       click_button 'Log in'
-      
-      
+
       expect(page).to have_content('Invalid Email or password')
       expect(page).to have_current_path(new_user_session_path, wait: 5)
     end
